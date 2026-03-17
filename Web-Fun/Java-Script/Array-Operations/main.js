74231f219b04d72417e7878a45f14e547e47885f
AhmadAbd2020 <ahmadabdhaleem@gmail.com>
2026-03-17 13:25:00 +0200

    profile-page

--- Web-Fun/Java-Script/Array-Operations/main.js
+++ Web-Fun/Java-Script/Array-Operations/main.js
@@ -87,4 +87,27 @@ function rotateArray(arr, k) {
   rotatedArray = rotatedArray.concat(slicedArray, remainingArray);
   console.log(rotatedArray);
 }
-rotateArray();
+function sortedArray(arr1, arr2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length && j < arr2.length) {
+    if (arr1[i] < arr2[j]) {
+      result.push(arr1[i]);
+      i++;
+    } else {
+      result.push(arr2[j]);
+      j++;
+    }
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i]);
+    i++;
+  }
+
+  while (j < arr2.length) {
+    result.push(arr2[j]);
+    j++;
+  }
+  return result;
+}
