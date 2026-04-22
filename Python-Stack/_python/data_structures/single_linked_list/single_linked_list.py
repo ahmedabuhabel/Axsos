193d9638e7bda6044aaabe8295ef513cec4ce3ca
AhmadAbd2020 <ahmadabdhaleem@gmail.com>
2026-04-22 08:11:53 +0300

    ninja bonus

--- Python-Stack/_python/data_structures/single_linked_list/single_linked_list.py
+++ Python-Stack/_python/data_structures/single_linked_list/single_linked_list.py
@@ -30,8 +30,43 @@ class SList:
         iterator.next = new_node
         return self
 
+    def remove_from_front(self):
+        if self.head == None:
+            return self
+        self.head = self.head.next
+        return self
+
+    def remove_from_back(self):
+        if self.head == None:
+            return self
+        if self.head.next == None:
+            self.head = None
+            return self
+        iterator = self.head
+        while iterator.next.next != None:
+            iterator = iterator.next
+
+        iterator.next = None
+        return self
+
+    def remove_val(self, val):
+        if self.head == None:
+            return self
+        if self.head.value == val:
+            self.head = self.head.next
+            return self
+        prev = self.head
+        iterator = self.head.next
+        while iterator != None:
+            if iterator.value == val:
+                prev.next = iterator.next
+                return self
+            prev = iterator
+            iterator = iterator.next
+        return self
+
 
 my_list = SList()
-my_list.add_to_front("are").add_to_front("Linked lists").add_to_back(
-    "fun!"
+my_list.add_to_front("are").add_to_front("Linked lists").add_to_back("fun!").remove_val(
+    "are"
 ).print_values()
